196a3033e7031ba2bdf21adea1beee86d8df3d0f
wanghuisong <wanghuisong@le.com>
2017-02-08 16:37:30 +0800

    Add 225, 232

---
+++ src/ImplementQueueUsingStacks.java
@@ -0,0 +1,59 @@
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/implement-queue-using-stacks/
+ *
+ * @author Wang Huisong(wanghuisong@le.com)
+ */
+public class ImplementQueueUsingStacks {
+    private Stack<Integer> inStack = new Stack<>();
+    private Stack<Integer> outStack = new Stack<>();
+
+    /** Initialize your data structure here. */
+    public ImplementQueueUsingStacks() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        inStack.add(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        while (inStack.size() != 0) {
+            outStack.add(inStack.pop());
+        }
+
+        int result = outStack.pop();
+
+        while (outStack.size() != 0) {
+            inStack.add(outStack.pop());
+        }
+
+        return result;
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        int result = 0;
+
+        while (inStack.size() != 0) {
+            outStack.add(inStack.pop());
+        }
+
+        result = outStack.peek();
+
+        while (outStack.size() != 0) {
+            inStack.add(outStack.pop());
+        }
+
+        return result;
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return inStack.isEmpty();
+    }
+}
---
+++ src/ImplementStackUsingQueues.java
@@ -0,0 +1,72 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/problems/implement-stack-using-queues/
+ *
+ * @author Wang Huisong(wanghuisong@le.com)
+ */
+public class ImplementStackUsingQueues {
+    private Queue<Integer> inQueue;
+    private Queue<Integer> outQueue;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public ImplementStackUsingQueues() {
+        inQueue = new ArrayDeque<Integer>();
+        outQueue = new ArrayDeque<Integer>();
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        inQueue.add(x);
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        int result = 0;
+        while (inQueue.size() > 1) {
+            outQueue.add(inQueue.poll());
+        }
+
+        if (inQueue.size() == 1) {
+            result = inQueue.poll();
+        }
+
+        while (outQueue.size() != 0) {
+            inQueue.add(outQueue.poll());
+        }
+
+        return result;
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        int result = pop();
+        push(result);
+
+        return result;
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return inQueue.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        ImplementStackUsingQueues obj = new ImplementStackUsingQueues();
+        obj.push(2131);
+        int param_2 = obj.pop();
+        int param_3 = obj.top();
+        boolean param_4 = obj.empty();
+    }
+}
